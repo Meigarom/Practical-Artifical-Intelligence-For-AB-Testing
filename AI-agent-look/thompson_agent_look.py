12a05fad9b0e3fff4bcca3ea73300ef7de1713f6
meigarom <meigaromlopes@gmail.com>
2019-02-16 20:45:54 -0200

    add Thompson Agent script

--- AI-agent-look/thompson_agent_look.py
+++ AI-agent-look/thompson_agent_look.py
@@ -5,6 +5,28 @@ from matplotlib import pyplot as plt
 import numpy as np
 import random
 
+# Reward Probability Plot function
+def reward_prob( success_array, failure_array ):
+    linestyles = ['-', '--']
+    x = np.linspace( 0, 1, 1002 )[1:-1]
+
+    plt.clf()
+    plt.xlim( 0, 1 )
+    plt.ylim( 0, 30 )
+
+    for a, b, ls in zip( success_array, failure_array, linestyles ):
+        dist = beta( a, b )
+
+        plt.plot( x, dist.pdf( x ), ls=ls, c='black', 
+                  label=r'$\alpha=%.1f,\ \beta=%.1f$' % ( a, b ) )
+        plt.draw()
+        plt.pause( 0.001 )
+
+        plt.legend( loc= 0 )
+
+    print( 'Success: {}'.format( success_array ) )
+    print( 'Failure: {}'.format( failure_array ) )
+
 # Thompson agent class
 class ThompsonAgent( object ):
     def __init__( self, prob_list ):
@@ -33,7 +55,7 @@ avg_accumulated_reward_array = list()
 # running experiment
 for episode in range( episodes ):
     if episode % 100 == 0:
-        print( 'episode: %s' % str( episode ) )
+        print( 'Episode: {}/{}'.format( episode, episodes ) )
 
     success_array = np.ones( len( prob_list ) )
     failure_array = np.ones( len( prob_list ) )
@@ -42,32 +64,8 @@ for episode in range( episodes ):
 
     # iterate over trials
     for trial in range( trials ):
-        print( 'Trials: {}/{}'.format( trial, trials ) )
-        linestyles = ['-', '--']
-        x = np.linspace( 0, 1, 1002 )[1:-1]
-
-        #fig, ax = plt.subplots( figsize=( 5, 3.75 ) )
-        plt.clf()
-        plt.xlim( 0, 1 )
-        plt.ylim( 0, 30 )
-
-        for a, b, ls in zip( success_array, failure_array, linestyles ):
-            dist = beta( a, b )
-
-            plt.plot( x, dist.pdf( x ), ls=ls, c='black', 
-                      label=r'$\alpha=%.1f,\ \beta=%.1f$' % ( a, b ) )
-            plt.draw()
-            plt.pause( 0.001 )
-
-            plt.legend( loc= 0 )
-
-        print( 'Success: {}'.format( success_array ) )
-        print( 'Failure: {}'.format( failure_array ) )
-
+        # Thompson strategy
         prob_reward = np.random.beta( success_array, failure_array )
-
-        print( 'Prob Reward: {}'.format( prob_reward ) )
-
         bandit_machine = np.argmax( prob_reward )
                 
         # get the reward
@@ -76,10 +74,8 @@ for episode in range( episodes ):
         # get results
         if reward == 1:
             success_array[ bandit_machine ] += 1
-        elif reward == 0:
-            failure_array[ bandit_machine ] += 1
         else:
-            raise Exception( 'Wrong value returned as Reward...' )
+            failure_array[ bandit_machine ] += 1
 
         # save partial results
         bandit_array[ bandit_machine ] += 1
